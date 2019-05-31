892009b58a727918be19550ce52788408862d1f5
Brad McGlasson <brad.mcglasson@onestop.com>
2019-05-30 17:13:21 -0700

    PDM-441 : JS : Fixing the ridiculous indention I used...

--- src/scripts/react-components/discounts/ModalRemoved.js
+++ src/scripts/react-components/discounts/ModalRemoved.js
@@ -1,43 +1,43 @@
 const PropTypes = require( 'prop-types' );
 
 class ModalRemoved extends React.Component {
-	constructor( props ){
-		super( props );
-	}
+  constructor( props ){
+    super( props );
+  }
 
-	render() {
-		const { 
-			confirmRemoval, 
-			removedDiscounts 
-		} = this.props;
+  render() {
+    const { 
+      confirmRemoval, 
+      removedDiscounts 
+    } = this.props;
 
-		const removedItems = removedDiscounts.map( discount => {
-			return(
-				<li className="modal-removal-item">
-					<div className="modal-removal-image" style={{ backgroundImage: `url(${discount.imageUrl || ''} )` }}></div>
-					<div className="modal-removal-title">{ discount.displayName || discount.title || 'Free Gift Item' }</div>
-				</li>
-			);
-		});
+    const removedItems = removedDiscounts.map( discount => {
+      return(
+        <li className="modal-removal-item">
+          <div className="modal-removal-image" style={{ backgroundImage: `url(${discount.imageUrl || ''} )` }}></div>
+          <div className="modal-removal-title">{ discount.displayName || discount.title || 'Free Gift Item' }</div>
+        </li>
+      );
+    });
 
-		
-		return (
-		  <div className="modal-item removed-discounts">
-			<div className="modal-item-removal-header">Your cart no longer meets the requirements for these deals:</div>
-			<ul className="modal-item-removal-list">
-				{ removedItems }
-			</ul>
-			<div className="modal-item-buttons">
-				<button className="modal-item-btn-confirm" onClick={ confirmRemoval }>OK</button>
-			</div>
-		  </div>
-		);
-	}
+    
+    return (
+      <div className="modal-item removed-discounts">
+      <div className="modal-item-removal-header">Your cart no longer meets the requirements for these deals:</div>
+      <ul className="modal-item-removal-list">
+        { removedItems }
+      </ul>
+      <div className="modal-item-buttons">
+        <button className="modal-item-btn-confirm" onClick={ confirmRemoval }>OK</button>
+      </div>
+      </div>
+    );
+  }
 }
 
 ModalRemoved.propTypes = {
-	confirmRemoval: PropTypes.func.isRequired,
-	removedDiscounts: PropTypes.array.isRequired
+  confirmRemoval: PropTypes.func.isRequired,
+  removedDiscounts: PropTypes.array.isRequired
 }
 
 module.exports = ModalRemoved;
