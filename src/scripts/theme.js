5902647a0d3e4e104632ff1da9dd173c4b1c198d
aakashZestard <aakash@zestard.net>
2019-11-07 18:23:21 +0530

    Mobile nav menu issue fixed

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -957,7 +957,7 @@ theme.Header = (function() {
     // MOBILE NAV : Attach menu toggle event
     if ( ui.mobileNavButton && ui.mobileNavMenu ) {
       ui.mobileNavButton.on( 'click', () => {
-        ui.mobileNavMenu.toggleClass( 'mobile-nav-open' ); // TOGGLE : Menu itself
+        ui.mobileNavMenu.toggleClass( 'mobile-nav-open' ); // TOGGLE : Menu itself 
         ui.body.toggleClass( 'js-drawer-open' ); // TOGGLE : Page scrolling (built in to a lib so tied to this classname)
         ui.html.toggleClass( 'menu-open' ); // TOGGLE : Html has some oddness from the theme, this clears it so iPoos can render right
       })
